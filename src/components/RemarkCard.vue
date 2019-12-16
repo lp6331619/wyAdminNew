6a65e8ea0a12ecc226929103fba9c9b5dd4f3b8b
jiangnan <jiangnan@wy.net>
2019-12-16 18:16:05 +0800

    更新

--- src/components/RemarkCard.vue
+++ src/components/RemarkCard.vue
@@ -1,8 +1,14 @@
 <template>
   <div v-if="dataBox">
     <el-card class="box-card p0">
-      <el-collapse v-model="activeNames" @change="handleChange">
-        <el-collapse-item title="备注信息" name="1">
+      <el-collapse v-model="activeNames">
+        <el-collapse-item title="备注信息" name="1" :accordion="true">
+          <template slot="title" class="p20">
+            备注信息
+            <el-button class="ml20" type="primary" size="mini" plain>
+              <i class="el-icon-circle-plus blue mr5"></i>添加
+            </el-button>
+          </template>
           <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
           <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
         </el-collapse-item>
@@ -13,7 +19,7 @@
 <script>
 // import { setPassword } from '@/api/member'
 export default {
-  name: 'Configurable',
+  name: 'RemarkCard',
   props: {
     initData: {
       type: Object,
@@ -32,18 +38,24 @@ export default {
     this.dataBox = this.initData
   },
   methods: {
-    emitOut(status) {
-      status === 'right' ? this.setPass() : (this.dataBox.display = false)
-    },
-    handleChange(e) {
-      console.log(e)
-    }
+    emitOut(status) {}
   }
 }
 </script>
 
-<style scoped lang="scss">
-.el-card__body {
-  padding: 0;
+<style lang="scss">
+.p0 {
+  .el-card__body {
+    padding: 0;
+  }
+  .el-collapse-item__header {
+    padding: 18px 20px;
+    border-bottom: 1px solid #ebeef5;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    font-size: 16px;
+    font-weight: normal;
+    height: 55px;
+  }
 }
 </style>
